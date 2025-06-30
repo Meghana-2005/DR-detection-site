47ce8c0fb451d87c0c0535584bc0d30dea2079fe
Meghana-2005 <138367936+Meghana-2005@users.noreply.github.com>
2025-06-30 14:56:27 +0530

    Update Home.jsx

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -5,7 +5,7 @@ const Home = () => {
     <div className='  bg-white pt-16 md:pt-24 text-orange-600'>
       <div className='flex-col justify-items-center   py-3 '>
       
-      <h className=''>VISION CARE , LIFE CARE</h>
+      <h>VISION CARE , LIFE CARE</h>
         <p className='text-center py-3'>A healthy lifestyle is key to managing diabetes, but what happens when diabetes silently damages your eyes? Diabetic Retinopathy (DR) is one of the leading causes of blindness among diabetic patients. Without early detection, it can progress undetected until significant vision loss occurs. Current diagnostic methods can be slow and costly, and sometimes, early signs are missed.
 
            Now, with the power of AI-driven technology, early detection is more accessible than ever. Our innovative system uses advanced AI based models to analyze retinal images, accurately identifying the stages of DR. This approach allows healthcare providers to diagnose the condition early, enabling timely treatment and reducing the risk of severe vision impairment. By offering faster and more reliable results, the model aims to improve patient outcomes and ease the workload on medical professionals.</p>
